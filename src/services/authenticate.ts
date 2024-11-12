7755cf2deb97b1cd9f0c98e2a167d8e9b25a8e6c
diegosmp <diiegosmp@gmail.com>
2024-11-12 19:28:01 -0300

    feature: message error credentials

--- src/services/authenticate.ts
+++ src/services/authenticate.ts
@@ -1,5 +1,21 @@
 import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUseCaseResponse = void
 
 export class AuthenticateUseCase {
   constructor(private usersRepository: UsersRepository) {}
+
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+  }
 }

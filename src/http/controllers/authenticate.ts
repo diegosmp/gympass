39e8ecd205a5fcde69c5f403b5a320511e674f6c
diegosmp <diiegosmp@gmail.com>
2024-12-08 15:53:32 -0300

    Feature: Authenticate | Refactory: Test Authenticate and register

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,31 @@
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { AuthenticateUseCase } from '@/services/authenticate'
+import { InvalidCredentialsError } from '@/services/errors/invalid-credentials-error'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticate(req: FastifyRequest, reply: FastifyReply) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(req.body)
+
+  try {
+    const usersRepository = new PrismaUsersRepository()
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+    await authenticateUseCase.execute({
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+
+  return reply.status(200).send()
+}
--- src/services/authenticate.spec.ts
+++ src/services/authenticate.spec.ts
@@ -1,13 +1,18 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, beforeEach } from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { AuthenticateUseCase } from './authenticate'
 import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+let userRepository: InMemoryUsersRepository
+let sut: AuthenticateUseCase
 
 describe('Authenticate Use Case', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUsersRepository()
+    sut = new AuthenticateUseCase(userRepository)
+  })
   it('should be able to authenticate', async () => {
-    const userRepository = new InMemoryUsersRepository()
-    const sut = new AuthenticateUseCase(userRepository)
-
     await userRepository.create({
       name: 'John Doe',
       email: 'johndoe@exemple.com',
@@ -20,5 +25,29 @@ describe('Authenticate Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    await userRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123123',
+      })
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
